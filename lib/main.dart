a1cb70109b3fe572393b22563093a7e68eb31b58
Doan Viet Thuan <thuan.dv@tasco.com.vn>
2025-03-09 23:46:22 +0700

    Widget trong Flutter: SafeArea, Scaffold, AppBar, Center, Text

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Text('Hello world')));
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('Tu hoc Flutter'),
+            backgroundColor: Colors.red,
+          ),
+          body: Center(
+            child: Text(
+              'Hello World',
+              style: TextStyle(fontSize: 18, color: Colors.black54),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
 }
